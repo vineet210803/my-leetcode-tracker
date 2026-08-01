40eb5e6f1891d760e61db6cd923e3494669829f3
weeeeneet <123195984+vineet210803@users.noreply.github.com>
2026-08-01 15:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.44%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int sol(vector<int>& v, int l, int r, vector<vector<int>>& dp) {
+        if (l > r)
+            return 0;
         if (l == r) {
             return v[l];
         }
         if(dp[l][r]!=-1) return dp[l][r];
-        int left = v[l] - sol(v, l + 1, r, dp);
-        int right = v[r] - sol(v, l, r - 1, dp);
-        return dp[l][r]= max(left, right);
+        int a = v[l] + min(sol(v, l + 2, r, dp), sol(v, l + 1, r - 1, dp));
+        int b = v[r] + min(sol(v, l + 1, r - 1, dp), sol(v, l, r - 2, dp));
+        return dp[l][r]= max(a,b);
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n, vector<int>(n,-1));
-        int l = 0;
-        int r = nums.size() - 1;
-        return sol(nums, l, r, dp) >= 0;
+        int n = nums.size();
+        int sum=0;
+        for(auto it:nums){
+            sum+=it;
+            
+        }
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int s1=sol(nums, 0, n-1, dp);
+        int s2=sum-s1;
+        return s1>=s2;
     }
 };
