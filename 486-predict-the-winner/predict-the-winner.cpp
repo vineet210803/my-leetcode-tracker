ed1073cd33cd67a6d274f5c4c2c710866bcd1e9e
weeeeneet <123195984+vineet210803@users.noreply.github.com>
2026-08-01 13:16:12 +0530

    Time: 96 ms (16.74%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-
-    bool sol(vector<int>& v, int l, int r, int a, int b){
-        if(l>r){
-            return a>=b;
+    int sol(vector<int>& v, int l, int r) {
+        if (l == r) {
+            return v[l];
         }
-        int turn= v.size()-(r-l+1);
-        if(turn%2==0){
-            bool f1= sol(v, l+1, r, a+v[l], b);
-            bool f2 = sol(v, l, r-1, a+v[r], b);
-            return f1||f2;
-        }else{
-            bool f3=sol(v, l+1, r, a, b+v[l]);
-            bool f4=sol(v, l, r-1, a, b+v[r]);
-            return f3&&f4;
-        }
+        int left = v[l] - sol(v, l + 1, r);
+        int right = v[r] - sol(v, l, r - 1);
+        return max(left, right);
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        int l=0;
-        int r=nums.size()-1;
-        int a=0;
-        int b=0;
-        return sol(nums, l, r, a, b);
+        
+        int l = 0;
+        int r = nums.size() - 1;
+        return sol(nums, l, r) >= 0;
     }
 };
