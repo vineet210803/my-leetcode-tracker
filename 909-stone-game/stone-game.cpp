98551e3e56b5cb1ad01486b52d2630adfc0108a0
weeeeneet <123195984+vineet210803@users.noreply.github.com>
2026-08-02 11:03:08 +0530

    Time: 17 ms (16.30%) | Memory: 19.7 MB (11.06%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int sol(vector<int>& piles, int l, int r, vector<vector<int>>& dp){
+
+    int sol(vector<int>& v, int l, int r, vector<vector<int>>& dp){
         if(l==r){
-            return piles[l];
+            return v[l];
         }
         if(l>r){
             return 0;
         }
         if(dp[l][r]!=-1) return dp[l][r];
 
-        int a= piles[l]-sol(piles, l+1, r, dp);
-        int b=piles[r]-sol(piles, l, r-1, dp);
-
-        return dp[l][r]=max(a,b);
+        int a= v[l]+min(sol(v, l+1, r-1, dp), sol(v, l+2, r, dp));
+        int b= v[r]+min(sol(v, l+1, r-1, dp), sol(v, l, r-2, dp));
+        return dp[l][r] = max(a,b);
     }
 
-    bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return sol(piles, 0, n-1, dp)>0;
+    bool stoneGame(vector<int>& v) {
+        int n=v.size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=v[i];
+        }
+        int alice= sol(v, 0, n-1 , dp);
+        int bob= sum-alice;
+        return alice>=bob;
     }
 };
